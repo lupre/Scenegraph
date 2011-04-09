a854c172fa7aac5fa0575bd26780780c178b5b7a
Klepel <daniel@klepel.de>
2011-04-09 11:04:16 +0200

    Initial Commit

---
+++ SceneGraph/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SceneGraph
+//
+//  Created by Daniel on 4/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
